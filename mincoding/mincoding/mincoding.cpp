f310d5c4778409f3a123dcd0a9c54d7ff27628fb
jeong-b <jeongb9716@gmail.com>
2023-01-27 15:50:42 +0900

    230127 수업끝

---
+++ mincoding/mincoding/mincoding.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int board[7][7] = {
+		0, 0, 0, 0, 0, 0, 0,
+		0, 0, 1, 0, 1, 0, 0,
+		0, 1, 2, 0, 2, 1, 0,
+		0, 0, 1, 2, 1, 0, 0,
+		0, 0, 2, 1, 0, 1, 0,
+		0, 1, 1, 0, 0, 0, 0,
+		0, 0, 0, 0, 0, 0, 0
+	};
+	int dir[4][2] = { { -1, 0 }, { 0, 1 }, { 1, 0 }, { 0, -1 } };
+	int inputY, inputX, cnt = 0;
+	cin >> inputY >> inputX;
+	board[inputY][inputX] = 1;
+	for (int i = 0; i < 7; i++)
+	{
+		for (int j = 0; j < 7; j++)
+		{
+			if (board[i][j] == 2)
+			{
+				for (int k = 0; k < 4; k++)
+				{
+					int ny = i + dir[k][0];
+					int nx = j + dir[k][1];
+					if (board[ny][nx] != 1) { break; }
+					cnt = 1;
+				}
+			}
+		}
+	}
+	cout << cnt;
+
+
+	return 0;
+}
