41085e160e59a31dd3bbc487a2611a6e9e2c2e1a
jeong-b <jeongb9716@gmail.com>
2023-02-01 17:48:47 +0900

    230201 수업끝

--- mincoding/mincoding/mincoding.cpp
+++ mincoding/mincoding/mincoding.cpp
@@ -1,43 +1,12 @@
 ﻿#include <iostream>
+#include <vector>
 using namespace std;
 
-char map[4][4];
-char monster[26][2];
-int dir[5][2] = { {0, 1}, {-1, 0}, {0, -1}, {1, 0}, {0, 1} };
-int dat[26];
-int monsterIndex;
-
-void input()
-{
-	for (int i = 0; i < 4; i++)
-	{
-		cin >> map[i];
-	}
-
-	for (int i = 0; i < 4; i++)
-	{
-		for (int j = 0; j < 3; j++)
-		{
-			if (map[i][j] >= 'A' && map[i][j] <= 'Z')
-			{
-				dat[map[i][j] - 'A']++;
-				monster[map[i][j] - 'A'][0] = i;
-				monster[map[i][j] - 'A'][1] = j;
-			}
-		}
-	}
-}
-
-void changePosition(int time, int number)
-{
-	if (time == 5) return;
-	
-}
+vector
 
 int main()
 {
-	input();
-	changePosition(0, 0);
+
 
 	return 0;
 }
