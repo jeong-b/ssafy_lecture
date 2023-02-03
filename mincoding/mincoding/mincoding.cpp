28041e8a357e2f47c03f872aeb4e55a90d90f778
jeong-b <jeongb9716@gmail.com>
2023-02-03 18:00:55 +0900

    230223 수업끝

--- mincoding/mincoding/mincoding.cpp
+++ mincoding/mincoding/mincoding.cpp
@@ -1,69 +1,74 @@
 ﻿#include <iostream>
-#include <cstring>
+#include <vector>
 using namespace std;
 
-char a[3][3], b[3][3];
-int cnt, flag;
+int map[5][4];
+int dirLine[3] = { 1, 2, 3 };
+vector<int> deleteLine;
+int flag;
 
-void turn(char arr[3][3])
-{
-	char turned[3][3];
-	turned[0][0] = arr[0][2];
-	turned[0][1] = arr[1][2];
-	turned[0][2] = arr[2][2];
-	turned[1][0] = arr[0][1];
-	turned[1][1] = arr[1][1];
-	turned[1][2] = arr[2][1];
-	turned[2][0] = arr[0][0];
-	turned[2][1] = arr[1][0];
-	turned[2][2] = arr[2][0];
-
-	for (int i = 0; i < 3; i++)
-	{
-		for (int j = 0; j < 3; j++)
-		{
-			arr[i][j] = turned[i][j];
-		}
-	}
-}
-
-void run(char c[3][3], char d[3][3])
-{
-	for (int i = 0; i < 3; i++)
-	{
-		if (strcmp(c[i], d[i]) == 0) flag += 1;
-		else flag = 0;
-	}
-	if (flag == 3)
-	{
-		cnt++;
-		return;
-	}
-	turn(c);
-	run(c, d);
-
-}
 
 int main()
 {
-	for (int i = 0; i < 3; i++)
+	for (int i = 0; i < 5; i++)
 	{
-		for (int j = 0; j < 3; j++)
+		for (int j = 0; j < 4; j++)
 		{
-			cin >> a[i][j];
+			cin >> map[i][j];
 		}
 	}
 
-	for (int i = 0; i < 3; i++)
+	for (int i = 0; i < 5; i++)
 	{
-		for (int j = 0; j < 3; j++)
+		if (map[i][0] == 1)
 		{
-			cin >> b[i][j];
+			for (int j = 0; j < 3; j++)
+			{
+				int nx = dirLine[j];
+				if (map[i][nx] == 0) 
+				{
+					flag = 0;
+					break;
+				}
+				flag = 1;
+			}
+		}
+
+		if (flag == 1)
+		{
+			map[i][0] = 0;
+			for (int j = 0; j < 3; j++)
+			{
+				int nx = dirLine[j];
+				map[i][nx] = 0;
+			}
+			deleteLine.push_back(i);
+		}
+		flag = 0;
+	}
+
+	for (int i = 0; i < deleteLine.size(); i++)
+	{
+		for (int j = deleteLine[i] - 1; j >= 0; j--)
+		{
+			for (int k = 0; k < 4; k++)
+			{
+				int ny = j + 1;
+				map[ny][k] = map[j][k];
+			}
 		}
 	}
 
-	run(a, b);
-	cout << cnt;
 
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = 0; j < 4; j++)
+		{
+			cout << map[i][j] << " ";
+		}
+		cout << "\n";
+	}
+
+	
 	return 0;
 }
