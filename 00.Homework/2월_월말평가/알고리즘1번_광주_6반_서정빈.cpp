3e51f9bbc73813a7ca1b36a2116af252a331635a
jeong-b <jeongb9716@gmail.com>
2023-02-27 16:48:58 +0900

    230227 수업정리

---
+++ 00.Homework/2월_월말평가/알고리즘1번_광주_6반_서정빈.cpp
@@ -0,0 +1,70 @@
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int nodeCnt, edgeCnt;
+int cnt;
+int used[101];
+vector<vector<int>> alist;
+
+void bfs(int start)
+{
+	// queue
+	queue<int> q;
+	q.push(start);
+
+	// used
+	used[start] = cnt;
+
+	// bfs
+	while (!q.empty())
+	{
+		int now = q.front();
+		q.pop();
+
+		for (int i = 0; i < alist[now].size(); i++)
+		{
+			int next = alist[now][i];
+
+			if (used[next] != 0) continue;
+
+			used[next] = cnt;
+			q.push(next);
+		}
+	}
+}
+
+int main()
+{
+	// reset
+	for (int i = 0; i < 101; i++) used[i] = 0;
+	cnt = 0;
+
+	// input
+	cin >> nodeCnt >> edgeCnt;
+	for (int i = 0; i <= nodeCnt; i++) alist.push_back({});
+	for (int i = 0; i < edgeCnt; i++)
+	{
+		int from, to;
+		cin >> from >> to;
+		alist[from].push_back(to);
+		alist[to].push_back(from);
+	}
+
+	// solve
+	for (int i = 1; i <= nodeCnt; i++)
+	{
+		if (used[i] != 0) continue;
+
+		cnt++;
+		used[i] = cnt;
+
+		bfs(i);
+	}
+
+	// output
+	cout << cnt;
+
+	return 0;
+}
---
+++ 00.Homework/2월_월말평가/알고리즘2번_광주_6반_서정빈.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+using namespace std;
+
+int numCnt;
+int maxValue, minValue;
+int num[11], calc[4];
+
+void run(int level, int result)
+{
+	// #2. 기저 조건
+	if (level == numCnt)
+	{
+		if (result > maxValue) maxValue = result;
+		if (result < minValue) minValue = result;
+
+		return;
+	}
+
+	for (int i = 0; i < 4; i++)
+	{
+		// #4. 가지 치기
+		if (calc[i] == 0) continue;
+
+		// #3. 기록 및 처리
+		calc[i] -= 1;
+
+		// #1. 재귀 기본 구조
+		if (i == 0) run(level + 1, result + num[level]);
+		if (i == 1) run(level + 1, result - num[level]);
+		if (i == 2) run(level + 1, result * num[level]);
+		if (i == 3) run(level + 1, result / num[level]);
+
+		// #5. 복구
+		calc[i] += 1;		
+	}
+}
+
+int main()
+{
+	// reset
+	maxValue = -21e8;
+	minValue = 21e8;
+
+	// input
+	cin >> numCnt;
+	for (int i = 0; i < numCnt; i++) cin >> num[i];
+	for (int i = 0; i < 4; i++) cin >> calc[i];
+
+	// solve
+	run(1, num[0]);
+
+	// output
+	cout << maxValue << '\n' << minValue;
+
+	return 0;
+}
